cac0e45d59239dc4f24a605af0b4b3dcf12d2790
Anil <anilverma7697650507@gmail.com>
2023-12-22 23:34:48 +0530

    clear file

--- script.js
+++ script.js
@@ -13,11 +13,6 @@ searchBtn.addEventListener("click", () => {
       .then((response) => response.json())
       .then((data) => {
         let myMeal = data.meals[0];
-        console.log(myMeal);
-        console.log(myMeal.strMealThumb);
-        console.log(myMeal.strMeal);
-        console.log(myMeal.strArea);
-        console.log(myMeal.strInstructions);
         let count = 1;
         let ingredients = [];
         // console.log(ingredients);
@@ -27,7 +22,6 @@ searchBtn.addEventListener("click", () => {
           if (i.startsWith("strIngredient") && myMeal[i]) {
             ingredient = myMeal[i];
             measure = myMeal[`strMeasure` + count];
-            
             count += 1;
             ingredients.push(`${measure} ${ingredient}`);
            
